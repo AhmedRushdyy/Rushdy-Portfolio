337f91bfedfd410caf668a8a735f5be70541b8d7
Ahmed Rushdy <153014781+AhmedRushdyy@users.noreply.github.com>
2024-10-30 18:36:06 +0300

    first

---
+++ data/index.ts
@@ -0,0 +1,199 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Working on Improving My Back-End Skills",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "KHOSOSY - Course selling App",
+    des: "the ultimate platform for accessing high-quality courses from top instructors across various fields. Whether you're looking to enhance your professional skills, dive into a new hobby, or advance your academic knowledge.",
+    img: "/khososy.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/mongodb-svgrepo-com.svg",
+      "/nodejs-logo-svgrepo-com.svg",
+    ],
+    link: "https://depi-graduation-project-98ep.vercel.app/",
+    github: "https://github.com/KhaledElshrief/DEPI---Graduation-Project",
+  },
+  {
+    id: 2,
+    title: "WorldWise - Travel App",
+    des: "Save Your travel locations and add notes to it",
+    img: "/world-wise.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://world-wise-seven-ruddy.vercel.app/",
+    github: "https://github.com/AhmedRushdyy/WorldWise",
+  },
+  {
+    id: 3,
+    title: "Infinite Elegance - e-commerce app",
+    des: "A Clothing Selling app.",
+    img: "/legance.png",
+    iconLists: [
+      "/html-5-svgrepo-com.svg",
+      "/css-3-svgrepo-com.svg",
+      "/javascript-svgrepo-com.svg",
+    ],
+    link: "https://ahmedrushdyy.github.io/InfiniteElegance/",
+    github: "https://github.com/AhmedRushdyy/InfiniteElegance",
+  },
+  {
+    id: 4,
+    title: "To Do List App",
+    des: "",
+    img: "/todolist.png",
+    iconLists: ["/re.svg", "/css-3-svgrepo-com.svg"],
+    link: "https://to-do-list-two-sepia.vercel.app/",
+    github: "https://github.com/AhmedRushdyy/ToDoList",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Ahmed Rushdy was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ahmed Rushdy's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ahmed Rushdy is the ideal partner.",
+    name: "Robert Johnson",
+    title: "UpWork Client",
+  },
+  {
+    quote:
+      "Collaborating with Ahmed Rushdy was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ahmed Rushdy's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ahmed Rushdy is the ideal partner.",
+    name: "Robert Johnson",
+    title: "UpWork Client",
+  },
+  {
+    quote:
+      "Collaborating with Ahmed Rushdy was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ahmed Rushdy's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ahmed Rushdy is the ideal partner.",
+    name: "Robert Johnson",
+    title: "UpWork Client",
+  },
+  {
+    quote:
+      "Collaborating with Ahmed Rushdy was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ahmed Rushdy's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ahmed Rushdy is the ideal partner.",
+    name: "Robert Johnson",
+    title: "UpWork Client",
+  },
+  {
+    quote:
+      "Collaborating with Ahmed Rushdy was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ahmed Rushdy's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ahmed Rushdy is the ideal partner.",
+    name: "Robert Johnson",
+    title: "UpWork Client",
+  },
+];
+
+export const companies = [];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Code Camp",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Team Leader For a final project in College",
+    desc: "Designed and developed web app for all platforms using React .",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment .",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/AhmedRushdyy",
+  },
+
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/ahmed-rushdyy/",
+  },
+];
